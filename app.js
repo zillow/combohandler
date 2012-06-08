0ce41819f97b8149cf4fd845caa8dd56d38ebf2d
Daniel Stockman <daniels@zillow.com>
2012-06-07 17:59:20 -0700

    Enable Cluster2 'stop' and 'shutdown' arguments from command line in demo app.
    Start the demo app:
        ./app.js &
    
    Stop (harsh):
        ./app.js stop
    
    Shutdown (graceful):
        ./app.js shutdown

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 // Cluster2 docs: http://ql-io.github.com/cluster2/
-var Cluster = require('cluster2');
+var Cluster = require('cluster2'),
+    args = process.argv.slice(2),
 
-var c = new Cluster();
+    c = new Cluster({
+        stop    : args.indexOf("stop") > -1,
+        shutdown: args.indexOf("shutdown") > -1
+    });
 
 c.listen(function (cb) {
     var server = require('./lib/server'),
